9d45d7c782fbf8a0585ced80f7ba0e8de4bed987
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2022-06-28 13:54:45 +0100

    Add actions summary

--- uyuni-health-check.py
+++ uyuni-health-check.py
@@ -36,12 +36,18 @@ def summary_db():
     channels = execute_db_query("select count(*) from rhnchannel")
     packages = execute_db_query("select count(*) from rhnpackage")
     systems = execute_db_query("select count(*) from rhnserver")
-    actions = execute_db_query("select count(*) from rhnaction")
+    actions = execute_db_query("select count(*) from rhnserveraction")
+    actions_last_day = execute_db_query("select * from rhnserveraction WHERE created >= NOW() - '1 day'::INTERVAL")
+    failed_actions_last_day = [x for x in actions_last_day if x["status"] == "3"]
+    completed_actions_last_day = [x for x in actions_last_day if x["status"] == "2"]
 
     print("* Total of channels: {}".format(channels[0]["count"]))
     print("* Total of packages: {}".format(packages[0]["count"]))
     print("* Total of systems: {}".format(systems[0]["count"]))
     print("* Total of actions: {}".format(actions[0]["count"]))
+    print("* Total of actions in last 24 hours: {}".format(len(actions_last_day)))
+    print("   - Failed: {}".format(len(failed_actions_last_day)))
+    print("   - Completed: {}".format(len(completed_actions_last_day)))
 
 summary_db()
 jobs = find_salt_jobs()
