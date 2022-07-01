03cde407e0fd7c91f3a5df0ed91112a496597fa7
Cédric Bosdonnat <cbosdonnat@suse.com>
2022-07-01 13:07:02 +0200

    Start cleaning the command line too and use Click and rich

--- src/uyuni_health_check/main.py
+++ src/uyuni_health_check/main.py
@@ -1,66 +1,60 @@
-from pprint import pprint
-
-from uyuni_health_exporter import UyuniDataGatherer
-
-gatherer = UyuniDataGatherer()
+import click
+from rich import print
 
 
-def find_salt_jobs() -> dict:
-    return runner.cmd("jobs.list_jobs")
-
-
-def summary_salt_jobs(jobs: dict) -> dict:
-    jobs = find_salt_jobs()
-    summary = {
-        "functions": {},
-        "total": 0,
-    }
-    for jid in jobs:
-        summary["functions"].setdefault(jobs[jid]["Function"], 0)
-        summary["functions"][jobs[jid]["Function"]] += 1
-        summary["total"] += 1
-    return summary
-
-
-def execute_db_query(query: str) -> list:
-    kwargs = {
-        "user": master_opts["postgres"]["user"],
-        "password": master_opts["postgres"]["pass"],
-        "host": master_opts["postgres"]["host"],
-        "port": master_opts["postgres"]["port"],
-        "maintenance_db": master_opts["postgres"]["db"],
-    }
-    return runner.cmd("salt.cmd", ["postgres.psql_query", query], kwarg=kwargs)
-
-
-def summary_db():
-    channels = execute_db_query("select count(*) from rhnchannel")
-    packages = execute_db_query("select count(*) from rhnpackage")
-    systems = execute_db_query("select count(*) from rhnserver")
-    actions = execute_db_query("select count(*) from rhnserveraction")
-    actions_last_day = execute_db_query(
-        "select * from rhnserveraction WHERE created >= NOW() - '1 day'::INTERVAL"
-    )
-    failed_actions_last_day = [x for x in actions_last_day if x["status"] == "3"]
-    completed_actions_last_day = [x for x in actions_last_day if x["status"] == "2"]
-
-    print("* Total of channels: {}".format(channels[0]["count"]))
-    print("* Total of packages: {}".format(packages[0]["count"]))
-    print("* Total of systems: {}".format(systems[0]["count"]))
-    print("* Total of actions: {}".format(actions[0]["count"]))
-    print("* Total of actions in last 24 hours: {}".format(len(actions_last_day)))
-    print("   - Failed: {}".format(len(failed_actions_last_day)))
-    print("   - Completed: {}".format(len(completed_actions_last_day)))
-
-
-def run():
+def show_data():
     """
-    Main entry point for the tool
+    Gather the data from the exporter and loki and display them
     """
-    summary_db()
-    jobs = find_salt_jobs()
-    pprint(summary_salt_jobs(jobs))
+    # TODO Gather the data
+    # TODO Display them!
+    print("[italic red]Data will soon be output here[/italic red]")
+
+
+def build():
+    """
+    Build the container images
+    """
+
+def deploy_exporter(server, port):
+    """
+    Deploy the prometheus exporter on the server
+    """
+
+def deploy_promtail(server, port):
+    """
+    Deploy promtail on the server
+    """
+
+def run_loki():
+    """
+    Run loki to aggregate the logs
+    """
+
+
+@click.command()
+@click.option("-p", default=22, help="server SSH port")
+@click.argument("server", help="server to connect to")
+def health_check(server, port):
+    """
+    Build the necessary containers, deploy them, get the metrics and display them
+
+    :param server: the server to connect to
+    :param port: the SSH port of the server
+    """
+    # TODO build the containers
+    build()
+
+    # TODO Deploy the exporter
+    deploy_exporter(server, port)
+
+    # TODO Deploy promtail and Loki
+    deploy_promtail(server, port)
+    run_loki()
+
+    # Gather and show the data
+    show_data()
 
 
 if __name__ == "__main__":
-    run()
+    health_check()
